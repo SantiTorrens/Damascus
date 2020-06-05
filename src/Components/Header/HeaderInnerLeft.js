f280813142714fb3908e4d6d72b760f40031e2a2
SantiTorrens <santi.duendes@gmail.com>
2020-06-05 13:47:41 -0300

    cambio nombre pag

--- src/Components/Header/HeaderInnerLeft.js
+++ src/Components/Header/HeaderInnerLeft.js
@@ -10,7 +10,7 @@ function HeaderInnerLeft() {
             className="title"
             title="go to Damascus"
           >
-            DAMASCUS
+            Damasco
           </a>
         </span>
       </div>
